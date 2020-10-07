95242a78c499f8deb63f45a7299b08709b4d05fb
omrfrks <omrfrks310387@gmail.com>
2020-10-07 23:26:25 +0300

    add red marker

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Button, Segment, Tab, List, Header, Image } from "semantic-ui-react";
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
 
 const EventDetailsComponent = (props) => {
+    const pointerIcon = new L.Icon({
+        iconUrl: 'https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:22ab8275-0380-44be-9bda-61aa91baa399;version=0?component_id=a26758b6-8aa6-4c4a-992c-256589600590&api_key=CometServer1&access_token=1602108496_urn%3Aaaid%3Asc%3AUS%3A22ab8275-0380-44be-9bda-61aa91baa399%3Bpublic_7eb007d7084938b7a3ab84275955e214f4be99fd',
+        iconRetinaUrl: 'https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:22ab8275-0380-44be-9bda-61aa91baa399;version=0?component_id=a26758b6-8aa6-4c4a-992c-256589600590&api_key=CometServer1&access_token=1602108496_urn%3Aaaid%3Asc%3AUS%3A22ab8275-0380-44be-9bda-61aa91baa399%3Bpublic_7eb007d7084938b7a3ab84275955e214f4be99fd',
+        iconAnchor: [5, 55],
+        iconSize: [35, 50]
+      })
+      
     const panes = [
         {
             menuItem: "DETAILS",
@@ -47,19 +55,18 @@ const EventDetailsComponent = (props) => {
     };
     const LocationComponent = () => {
         const { event } = props;
-        const position = [event.location.latitude, event.location.longitude];
+        const position = [event?.location?.latitude, event?.location?.longitude];
         return (
             <Segment>
                 <Header as='h3' textAlign="left">Address</Header>
-                <Map center={position} zoom={13} style={{ height: "200px" }}>
+                {event?.location ? <Map center={position} zoom={13} style={{ height: "200px" }}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     />
-                    <Marker position={position}>
-                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                    <Marker  position={position} icon={pointerIcon} >
                     </Marker>
-                </Map>
+                </Map> : <p>There is no map information</p>}
             </Segment>
         );
     };
@@ -67,12 +74,12 @@ const EventDetailsComponent = (props) => {
         const { event } = props;
         return (
             <Segment>
-                {event?.media ? event.media[0].type === "image" ? <Image src={event?.media[0].url} size='large' />  : <audio
+                {event?.media ? event.media[0].type === "image" ? <Image src={event?.media[0].url} size='large' /> : <audio
                     controls
                     src={event?.media[0].url} >
                     Your browser does not support the
             <code>audio</code> element.
-    </audio> : <p>No Media Content</p> }
+    </audio> : <p>No Media Content</p>}
             </Segment>
         )
     };
